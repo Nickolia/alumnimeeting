87da71f5079068f1cbc96e1ffb80908e8e774fa1
Xopxe <xopxem@gmail.com>
2015-12-04 16:23:47 +0300

    update

---
+++ auth/index.js
@@ -0,0 +1,33 @@
+var passport = require('passport');
+var AuthVKStrategy = require('passport-vkontakte').Strategy;
+
+passport.use('vk', new AuthVKStrategy({
+        clientID:     "5174787",
+        clientSecret: "O0RcVhXduMn0tgVj4DAJ",
+        callbackURL:  "http://5.101.117.224:4300/auth/vkontakte/callback"
+    },
+    function (accessToken, refreshToken, profile, done) {
+
+        return done(null, {
+            username: profile.displayName,
+            photoUrl: profile.photos[0].value,
+            profileUrl: profile.profileUrl
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, JSON.stringify(user));
+});
+
+
+passport.deserializeUser(function (data, done) {
+    try {
+        done(null, JSON.parse(data));
+    } catch (e) {
+        done(err)
+    }
+});
+
+module.exports = function (app) {
+};
