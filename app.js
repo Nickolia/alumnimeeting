288b03c2543c227ecf08b398084fe5f2393391fd
Xopxe <xopxem@gmail.com>
2015-12-04 16:04:31 +0300

    update

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     multer = require('multer'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    VKontakteStrategy = require('passport-vkontakte').Strategy,
     _ = require('lodash-node');
 
 var app = module.exports = express();
@@ -27,6 +29,18 @@ mongoose.connect('mongodb://localhost:27017/alumnimeeting', function (error) {
     }
 });
 
+passport.use(new VKontakteStrategy({
+        clientID:     "5174787",
+        clientSecret: "O0RcVhXduMn0tgVj4DAJ",
+        callbackURL:  "http://5.101.117.224:4300/auth/vkontakte/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ vkontakteId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
+
 var init = function(){
     app.all(allowCrossDomain);
     app.use(logger('dev'));
@@ -39,6 +53,19 @@ var init = function(){
     io = require('socket.io').listen(server);
 
     io.on('connection', Connect);
+
+    app.get('/auth/vkontakte',
+        passport.authenticate('vkontakte'),
+        function(req, res){
+           console.log(req);
+        });
+
+    app.get('/auth/vkontakte/callback',
+        passport.authenticate('vkontakte', { failureRedirect: '/login' }),
+        function(req, res) {
+            console.log(req);
+            res.redirect('/');
+        });
 
 };
 
